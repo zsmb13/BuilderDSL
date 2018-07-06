aaedcaff8f3e4977f337f634e0440011812c4b3d
zsmb13 <braunmarci@gmail.com>
2018-07-06 20:47:48 +0200

    Added MaterialDrawer dependency

--- app/src/main/java/co/zsmb/builderdsl/MainActivity.kt
+++ app/src/main/java/co/zsmb/builderdsl/MainActivity.kt
@@ -2,12 +2,17 @@ package co.zsmb.builderdsl
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.mikepenz.materialdrawer.DrawerBuilder
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        DrawerBuilder()
+                .withActivity(this)
+                .build()
     }
 
 }
